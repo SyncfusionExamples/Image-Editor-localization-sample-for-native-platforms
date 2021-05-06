208da03a8249037ea122dc1b47bfafb2d6e8d23c
AzureAD\SrideviSivakumar <AzureAD\sridevi.sivakumar@syncfusion.com>
2021-05-06 11:20:39 +0530

    [XAMARINANDROID-3709] Localization Sample committed

---
+++ Localization_Android/EditorSample/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Syncfusion.SfImageEditor.Android;
+using Android.Graphics;
+
+namespace EditorSample
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SfImageEditor editor = new SfImageEditor(this);
+            editor.Bitmap = BitmapFactory.DecodeResource(Resources, Resource.Drawable.Image);
+            SetContentView(editor);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
